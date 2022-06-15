c3ecf1b86102dfd6f499c39fe1c4c20fc47ab4a6
Kushal Dulani <kushal@Kushals-MacBook.local>
2022-06-16 05:07:04 +0530

    schemas.py added

--- blog/main.py
+++ blog/main.py
@@ -1,15 +1,11 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from . import schemas
+
 
 app=FastAPI()
 
 
-class Blog(BaseModel):
-    title:str
-    body:str
-
-
 @app.post("/")
-def create(request:Blog):
+def create(request:schemas.Blog):
     return request
     
