e81b68583ec056ba0614372ba704f502aeb0f6ec
Kushal Dulani <kushal@Kushals-MacBook.local>
2022-06-15 18:34:45 +0530

    add post methods added

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+class Blog(BaseModel):
+    title:str
+    body:str
+    published:Optional[bool]
+
 
 app = FastAPI(title="Kushal")
 
@@ -6,6 +14,26 @@ app = FastAPI(title="Kushal")
 def index():
     return {"Status": "API is Up and Running 200 ok"}
 
-@app.get('/about')
+@app.get('/blog')
+def about(limit=10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {"data": f"{limit} published of blogs"}
+    else:
+        return {"data": f"{limit} of blogs"}
+
+@app.get('/blog/unpublished')
 def about():
-    return {"About": "Kushal"}
+    return {"data": "unpublished blogs"}
+
+@app.get('/blog/{id}')
+def about(id:int):
+    return {"data": id}
+
+@app.get('/blog/{id}/comments')
+def about(id:int, limit=10):
+    return limit
+    return {"data": "comments here"}
+
+@app.post('/blog')
+def create_blog(blog:Blog):
+    return {"data": f"blog is created title as {blog.title} and body as '{blog.body}'"}
